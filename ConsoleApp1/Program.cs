12967f5642c8e2c353e7b7685f63c37d89de8893
Shariful Islam <1931599@champlaincollege.qc.ca>
2020-09-09 13:56:15 -0400

    updated 1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Shariful";
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+            Console.WriteLine("This is a test, my name is {0}", myName);
+            Console.ReadLine();
+        }
+    }
+}
